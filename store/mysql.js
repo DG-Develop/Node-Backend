5036bada69168e8a515cc6a22c5dcbcb846e431d
DGDBAND <dgdband@hotmail.com>
2020-10-15 19:01:18 -0500

    Completando la base de datos

--- store/mysql.js
+++ store/mysql.js
@@ -19,7 +19,7 @@ function handleCon(params) {
         if (err) {
             console.error('[db err]', err)
             setTimeout(handleCon, 2000)
-        }else{
+        } else {
             console.log('DB Connected!')
         }
     })
@@ -36,18 +36,76 @@ function handleCon(params) {
 
 handleCon()
 
-function list (table){
-    console.log(table)
+function list(table) {
     return new Promise((resolve, reject) => {
-        conection.query(`SELECT * FROM ${table}`, 
+        conection.query(`SELECT * FROM ${table}`,
             (err, data) => {
-                if(err) return reject(err)
+                if (err) return reject(err)
                 resolve(data)
             }
         )
     })
 }
 
+function get(table, id) {
+    return new Promise((resolve, reject) => {
+        conection.query(`SELECT * FROM ${table} WHERE id=${id}`,
+            (err, data) => {
+                if (err) return reject(err)
+                resolve(data)
+            }
+        )
+    })
+}
+
+function insert(table, data) {
+    return new Promise((resolve, reject) => {
+        conection.query(`INSERT INTO ${table} SET ?`, data,
+            (err, result) => {
+                if (err) return reject(err)
+                resolve(result)
+            }
+        )
+    })
+}
+
+function update(table, data) {
+    return new Promise((resolve, reject) => {
+        conection.query(`UPDATE ${table} SET ? WHERE id=?`, [data, data.id],
+            (err, result) => {
+                if (err) return reject(err)
+                resolve(result)
+            }
+        )
+    })
+}
+
+function upsert(table, data) {
+    if (data && data.id) {
+        return update(table, data)
+    } else {
+        return insert(table, data)
+    }
+}
+
+function query(table, query) {
+    return new Promise((resolve, reject) => {
+        conection.query(`SELECT * FROM ${table} WHERE ?`, query,
+            (err, res) => {
+                if (err) return reject(err)
+                const data = {
+                    id: res[0].id,
+                    username: res[0].username,
+                    password: res[0].password,
+                }
+                resolve(data || null)
+            })
+    })
+}
+
 module.exports = {
-    list
+    list,
+    get,
+    upsert,
+    query
 }
