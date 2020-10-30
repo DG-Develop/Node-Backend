822a9d8f47e04f9779a210901787244512c090e4
DGDBAND <dgdband@hotmail.com>
2020-10-29 18:28:04 -0600

    Separando los posts a un microservicio

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ module.exports = {
     api: {
         port: process.env.API_PORT || 3000,
     },
+    post: {
+        port: process.env.POST_PORT || 3003
+    },
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!'
     },
